c4eac57e04bfabab0542cc4aa24ebc9538dfb855
Lloyd <lloyd1nspace@users.noreply.github.com>
2017-06-28 11:56:05 -0400

    Creating pod

---
+++ Example/Pods/Target Support Files/Pods-URBNSwiftyConvenience_Example/Pods-URBNSwiftyConvenience_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_URBNSwiftyConvenience_Example : NSObject
+@end
+@implementation PodsDummy_Pods_URBNSwiftyConvenience_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-URBNSwiftyConvenience_Tests/Pods-URBNSwiftyConvenience_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_URBNSwiftyConvenience_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_URBNSwiftyConvenience_Tests
+@end
