ce6e90a145d3e899d018a553e350760ce41622b3
Lloyd <lloyd1nspace@users.noreply.github.com>
2017-06-29 11:18:11 -0400

    UIColorConvenience file

--- URBNSwiftyConvenience/Classes/UIColorConvenience.swift
+++ URBNSwiftyConvenience/Classes/UIColorConvenience.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+extension UIColor {
+    public static func randomColor() -> UIColor {
+        let hue: CGFloat = CGFloat(arc4random() % 256) / 256
+        let saturation: CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
+        let brightness: CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
+        
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+    }
+}
