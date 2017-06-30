46e52edebdc380643152608f4d628460346017fe
Lloyd <lloyd1nspace@users.noreply.github.com>
2017-06-30 10:17:22 -0400

    Added URBNBorder class plus file

---
+++ URBNSwiftyConvenience/Classes/URBNBorders.swift
@@ -0,0 +1,16 @@
+//
+//  URBNBorders.swift
+//  Pods
+//
+//  Created by Lloyd Sykes on 6/30/17.
+//
+//
+
+import Foundation
+
+public class URBNBorder: NSObject {
+    let color = UIColor()
+    let width = CGFloat()
+    let insets = UIEdgeInsets()
+}
+
