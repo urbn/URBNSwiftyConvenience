fe5ac753437f2f053a699ede68edd55e084ee945
Lloyd <lloyd1nspace@users.noreply.github.com>
2017-06-30 10:50:49 -0400

    Added KVO functions on URBNBorderView

--- URBNSwiftyConvenience/Classes/URBNBorders.swift
+++ URBNSwiftyConvenience/Classes/URBNBorders.swift
@@ -20,7 +20,31 @@ fileprivate class URBNBorderView: UIView {
     let urbn_rightBorder: URBNBorder?
     let urbn_bottomBorder: URBNBorder?
     
+    private final let width = "width"
+    private final let color = "color"
+    private final let insets = "insets"
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+   
+    // MARK: KVO
+    func configuredBorder() -> URBNBorder {
+        let border = URBNBorder()
+        border.addObserver(self, forKeyPath: width, options: .new, context: nil)
+        border.addObserver(self, forKeyPath: color, options: .new, context: nil)
+        border.addObserver(self, forKeyPath: insets, options: .new, context: nil)
+        
+        return border
+    }
+    
+    func unregisterKVO(forBorder border: URBNBorder) {
+        border.removeObserver(self, forKeyPath: width)
+        border.removeObserver(self, forKeyPath: color)
+        border.removeObserver(self, forKeyPath: insets)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        setNeedsDisplay()
     }
 }
