090dba7befee2b114685fe5519cda07203e4c607
Matt Thomas <matt@codecaffeine.com>
2017-09-11 12:00:53 -0400

    Awesomer colors from hex values
    YOu’re welcome

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -4,26 +4,12 @@ import URBNSwiftyConvenience
 
 class Tests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testColors() {
+        XCTAssertEqual(UIColor(rgb: 0xFF0000), UIColor.red)
+        XCTAssertEqual(UIColor(rgb: 0x00FF00), UIColor.green)
+        XCTAssertEqual(UIColor(rgb: 0x0000FF), UIColor.blue)
+        XCTAssertEqual(UIColor(rgb: 0x00FFFF), UIColor.cyan)
+        XCTAssertEqual(UIColor(rgb: 0xFFFF00), UIColor.yellow)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
+
--- URBNSwiftyConvenience/Classes/UIColorConvenience.swift
+++ URBNSwiftyConvenience/Classes/UIColorConvenience.swift
@@ -9,6 +9,41 @@
 import Foundation
 
 extension UIColor {
+    
+    public convenience init(redInt: UInt8, greenInt: UInt8, blueInt: UInt8, alphaInt: UInt8 = 0xFF) {
+        let r = (CGFloat(redInt) / 255.0)
+        let g = (CGFloat(greenInt) / 255.0)
+        let b = (CGFloat(blueInt) / 255.0)
+        let a = (CGFloat(alphaInt) / 255.0)
+        
+        self.init(red: r, green: g, blue: b, alpha: a)
+    }
+    
+    public convenience init(rgb: Int) {
+        if rgb > 0xFFFFFF { // outside RGB
+            self.init(white: 0.0, alpha: 1.0)
+        }
+        else {
+            let r = UInt8((rgb & 0xFF0000) >> 16)
+            let g = UInt8((rgb & 0x00FF00) >> 8)
+            let b = UInt8(rgb & 0x0000FF)
+            self.init(redInt: r, greenInt: g, blueInt: b)
+        }
+    }
+    
+    public convenience init(rgba: Int) {
+        if rgba > 0xFFFFFFFF { // outside RGBA
+            self.init(white: 0.0, alpha: 1.0)
+        }
+        else {
+            let r = UInt8((rgba & 0xFF000000) >> 24)
+            let g = UInt8((rgba & 0x00FF0000) >> 16)
+            let b = UInt8((rgba & 0x0000FF00) >> 8)
+            let a = UInt8(rgba & 0x000000FF)
+            self.init(redInt: r, greenInt: g, blueInt: b, alphaInt: a)
+        }
+    }
+    
     public static func randomColor() -> UIColor {
         let hue: CGFloat = CGFloat(arc4random() % 256) / 256
         let saturation: CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
