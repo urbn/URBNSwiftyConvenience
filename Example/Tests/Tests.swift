fafebac0029f6770c9f449b03e5575856855857a
Matt Thomas <matt@codecaffeine.com>
2017-09-19 09:03:44 -0400

    add hexString support to UIColor

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -10,6 +10,16 @@ class Tests: XCTestCase {
         XCTAssertEqual(UIColor(rgb: 0x0000FF), UIColor.blue)
         XCTAssertEqual(UIColor(rgb: 0x00FFFF), UIColor.cyan)
         XCTAssertEqual(UIColor(rgb: 0xFFFF00), UIColor.yellow)
+        
+        XCTAssertEqual(UIColor(hexString: "FF0000"), UIColor.red)
+        XCTAssertEqual(UIColor(hexString: "#00FF00"), UIColor.green)
+        XCTAssertEqual(UIColor(hexString: "0000FF"), UIColor.blue)
+        XCTAssertEqual(UIColor(hexString: "#00FFFF"), UIColor.cyan)
+        XCTAssertEqual(UIColor(hexString: "FFFF00"), UIColor.yellow)
+
+        XCTAssertEqual(UIColor(hexString: "FFFF0"), UIColor(rgb: 0))
+        XCTAssertEqual(UIColor(hexString: "#FFFF0"), UIColor(rgb: 0))
+        XCTAssertEqual(UIColor(hexString: "Jerkface"), UIColor(rgb: 0))
     }
 }
 
