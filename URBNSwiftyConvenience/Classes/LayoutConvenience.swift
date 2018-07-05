7361493c19a4e5b5fb154a11e6def2a6f9fbb0a1
Matt Thomas <matt@codecaffeine.com>
2018-07-05 15:22:17 -0400

    removing unneeded function

--- URBNSwiftyConvenience/Classes/LayoutConvenience.swift
+++ URBNSwiftyConvenience/Classes/LayoutConvenience.swift
@@ -133,37 +133,6 @@ public extension UIStackView {
     }
 }
 
-//: Credit to [Adam Sharp](https://twitter.com/sharplet) & [Daniel Jalkut](https://twitter.com/danielpunkass)
-extension UIEdgeInsets: ExpressibleByDictionaryLiteral {
-    public typealias Key = EdgeKey
-    public typealias Value = CGFloat
-    
-    public enum EdgeKey {
-        case top
-        case left
-        case bottom
-        case right
-    }
-    
-    func keyPathForEdge(_ edgeKey: EdgeKey) -> WritableKeyPath<UIEdgeInsets, CGFloat> {
-        switch edgeKey {
-        case .top: return \UIEdgeInsets.top
-        case .left: return \UIEdgeInsets.left
-        case .bottom: return \UIEdgeInsets.bottom
-        case .right: return \UIEdgeInsets.right
-        }
-    }
-    
-    public init(dictionaryLiteral elements: (EdgeKey, CGFloat)...) {
-        self = UIEdgeInsets()
-        
-        for (inset, value) in elements {
-            let keyPath = self.keyPathForEdge(inset)
-            self[keyPath: keyPath] = value
-        }
-    }
-}
-
 // TODO: Refactor out when we move to swifty 4
 //       Also we could adopt add & subtract capability similar to - https://useyourloaf.com/blog/easier-swift-layout-priorities/
 @available(swift, deprecated: 4.0)
