57b35ecd9c5e58dc59c52eda21428a51c189c2af
JohnBueno <jmccartney@urbn.com>
2018-10-09 14:47:13 -0400

    added more tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -33,5 +33,30 @@ class Tests: XCTestCase {
         XCTAssertNil(myDictionary["foo"])
         XCTAssertEqual(myDictionary["bar"], "baz")
     }
+    
+    func testStableSort() {
+        struct Person: Equatable {
+            let name: String
+            let age: Int
+            
+            static func == (lhs: Person, rhs: Person) -> Bool {
+                return lhs.name == rhs.name && lhs.age == rhs.age
+            }
+        }
+        
+        let kristin = Person(name: "Kristin", age: 31)
+        let steve = Person(name: "Steve", age: 34)
+        let john = Person(name: "John", age: 34)
+        let dave = Person(name: "Dave", age: 40)
+        let bob = Person(name: "Bob", age: 32)
+        let joe = Person(name: "Joe", age: 32)
+        
+        let people = [dave, kristin, steve, john, bob, joe]
+        let ascSorted = people.stableSorted(by: { $0.age < $1.age })
+        let descSorted = people.stableSorted(by: { $0.age > $1.age })
+        
+        XCTAssert([kristin, bob, joe, steve, john, dave] == ascSorted)
+        XCTAssert([dave, steve, john, bob, joe, kristin] == descSorted)
+    }
 }
 
--- URBNSwiftyConvenience/Classes/Array+Extensions.swift
+++ URBNSwiftyConvenience/Classes/Array+Extensions.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
-extension Array {
-    
+extension RandomAccessCollection {
+    /// From: https://stackoverflow.com/a/45585365/1541526
+    /// return a sorted collection
+    /// this use a stable sort algorithm
+    ///
+    /// - Parameter areInIncreasingOrder: return nil when two element are equal
+    /// - Returns: the sorted collection
+    public func stableSorted(by areInIncreasingOrder: (Iterator.Element, Iterator.Element) -> Bool?) -> [Iterator.Element] {
+        
+        let sorted = self.enumerated().sorted { (one, another) -> Bool in
+            if let result = areInIncreasingOrder(one.element, another.element) {
+                return result
+            } else {
+                return one.offset < another.offset
+            }
+        }
+        
+        return sorted.map{ $0.element }
+    }
 }
